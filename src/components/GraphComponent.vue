2df443ec4f3f3beb73604f4554c122a055b22378
Radu Coanda <andrei.coanda@gmail.com>
2019-12-11 11:58:11 +0100

    Browse Function and Mine and Upload butttons

---
+++ src/components/GraphComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>Links: {{links}}</p>
+    <p>Nodes: {{nodes}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GraphComponent",
+  props: {
+    links: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    },
+    nodes: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
--- src/views/Process.vue
+++ src/views/Process.vue
@@ -1,34 +1,58 @@
 <template>
-  <span>
+  <span class="wrapper">
     <v-navigation-drawer :width="400" v-model="drawer" app clipped>
       <v-list dense nav>
-        <v-list-group v-if="this.labels" prepend-icon="mdi-filter" value="true">
-          <template v-slot:activator>
-            <v-list-item-title>Filters</v-list-item-title>
-          </template>
-          <v-list-item v-for="(activity, i) in this.labels" :key="i" link>
-            <v-list-item-title v-text="activity"></v-list-item-title>
+        <v-subheader>Actions</v-subheader>
+        <v-list-item-group color="primary">
+          <v-list-item @click="mine">
+            <!-- <v-list-item-icon>
+            <v-icon v-text="'item.icon'"></v-icon>
+            </v-list-item-icon>-->
 
-            <v-btn-toggle mandatory rounded dense v-model="filters[i]">
-              <v-btn>
-                <v-icon>mdi-asterisk</v-icon>
-              </v-btn>
-
-              <v-btn>
-                <v-icon>mdi-adjust</v-icon>
-              </v-btn>
-
-              <v-btn>
-                <v-icon>mdi-cancel</v-icon>
-              </v-btn>
-            </v-btn-toggle>
+            <v-list-item-content>
+              <v-list-item-title v-text="'Mine'"></v-list-item-title>
+            </v-list-item-content>
           </v-list-item>
-        </v-list-group>
+          <v-list-item>
+            <router-link :to="{name: 'home'}">
+              <!-- <v-list-item-icon>
+            <v-icon v-text="'item.icon'"></v-icon>
+              </v-list-item-icon>-->
 
+              <v-list-item-content>
+                <v-list-item-title v-text="'Upload'"></v-list-item-title>
+              </v-list-item-content>
+            </router-link>
+          </v-list-item>
+        </v-list-item-group>
+        <v-subheader>Settings</v-subheader>
         <v-list-group prepend-icon="mdi-card-search">
           <template v-slot:activator>
-            <v-list-item-title>Browse</v-list-item-title>
+            <v-list-item-title>Search</v-list-item-title>
           </template>
+
+          <v-list-group no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-title>Filters</v-list-item-title>
+            </template>
+            <v-list-item v-for="(activity, i) in labels" :key="i" link>
+              <v-list-item-title v-text="activity"></v-list-item-title>
+
+              <v-btn-toggle mandatory rounded dense v-model="filters[i]">
+                <v-btn>
+                  <v-icon>mdi-asterisk</v-icon>
+                </v-btn>
+
+                <v-btn>
+                  <v-icon>mdi-adjust</v-icon>
+                </v-btn>
+
+                <v-btn>
+                  <v-icon>mdi-cancel</v-icon>
+                </v-btn>
+              </v-btn-toggle>
+            </v-list-item>
+          </v-list-group>
 
           <v-list-group no-action sub-group>
             <template v-slot:activator>
@@ -39,18 +63,23 @@
 
             <v-list-item v-for="(a, i) in labels" :key="i" link>
               <v-list-item-title v-text="a"></v-list-item-title>
+              <v-list-item-action>
+                <v-checkbox v-model="visibleActivities[i]" color="primary"></v-checkbox>
+              </v-list-item-action>
             </v-list-item>
           </v-list-group>
 
-          <v-list-group sub-group no-action>
+          <v-list-group sub-group>
             <template v-slot:activator>
               <v-list-item-content>
                 <v-list-item-title>View Relationships</v-list-item-title>
               </v-list-item-content>
             </template>
-            <v-list-item v-for="(crud, i) in relationships" :key="i" @click>
-              <v-list-item-title v-text="crud"></v-list-item-title>
-            </v-list-item>
+            <v-list-item-group v-model="curRel" v-on:change="computeLinks">
+              <v-list-item v-for="(rel, i) in relationships" :key="i">
+                <v-list-item-title v-text="rel[0]"></v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
           </v-list-group>
         </v-list-group>
       </v-list>
@@ -61,34 +90,45 @@
       <v-toolbar-title>skelevision</v-toolbar-title>
     </v-app-bar>
 
-    <v-content>
+    <v-content class="main-content">
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
-          <v-col class="shrink">visualization</v-col>
+          <v-col>
+            <graph-component v-if="data" :nodes="activeNodes" :links="visibleLinks" />
+          </v-col>
         </v-row>
-        <v-btn @click="mine" absolute dark fab bottom right color="pink">
-          <v-icon>mdi-pickaxe</v-icon>
-        </v-btn>
       </v-container>
     </v-content>
   </span>
 </template>
 
 <script>
+import GraphComponent from "../components/GraphComponent.vue";
 export default {
   name: "process",
   data: () => ({
+    // UI
+    drawer: null,
+    fab: false,
+
+    // Data
     data: null,
+
+    // Nodes (Labels)
     labels: [],
     filters: [],
-    drawer: null,
+    visibleActivities: [],
+
+    // Relationships
     relationships: [
-      "Equivalence",
-      "Never Together",
-      "Always After",
-      "Always Before",
-      "Dependecy"
-    ]
+      ["Equivalence", "equivalence"],
+      ["Never Together", "neverTogether"],
+      ["Always After", "alwaysAfter"],
+      ["Always Before", "alwaysBefore"],
+      ["Dependecy", "dependency"]
+    ],
+    curRel: 0,
+    visibleLinks: []
   }),
   methods: {
     mine() {
@@ -110,7 +150,10 @@ export default {
       this.$http
         .post("mine", payload)
         .then(response => {
-          console.log(response);
+          if (response.status == 200) {
+            this.data = response.data;
+            this.computeLinks();
+          }
         })
         .catch(() => {
           console.log("FAILURE!!");
@@ -123,20 +166,49 @@ export default {
           if (response.status == 200) {
             this.labels = response.data.labels;
             this.filters = [];
+            this.visibleActivities = [];
             for (const a in this.labels) {
               this.filters.push(1);
+              this.visibleActivities.push(true);
             }
           }
         })
         .catch(() => {
           console.log("FAILURE!!");
         });
+    },
+    computeLinks() {
+      this.visibleLinks = [];
+      if (this.data) {
+        const i = this.relationships[this.curRel][1];
+        this.visibleLinks = this.data.relationships[i];
+      }
+      return this.visibleLinks;
     }
   },
   created: function() {
-    // this.getData();
     this.getLabels();
   },
-  components: {}
+  computed: {
+    activeNodes: function() {
+      const nodes = [];
+      for (var i = 0; i < this.labels.length; i++) {
+        if (this.visibleActivities[i]) {
+          nodes.push(this.labels[i]);
+        }
+      }
+      // TODO: Add node information
+      return nodes;
+    }
+  },
+  components: { GraphComponent }
 };
 </script>
+
+<style lang="sass" scoped>
+span.wrapper
+  height: 100%
+
+.main-content
+  height: calc(100%)
+</style>
