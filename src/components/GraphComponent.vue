1fc98a6cbf62a35b057e8d26552ee28f0c1ec64e
Hilke <hilke.buss@rwth-aachen.de>
2020-01-08 20:52:30 +0100

    Added custom link, changed node position

--- src/components/GraphComponent.vue
+++ src/components/GraphComponent.vue
@@ -38,26 +38,37 @@ export default {
   methods: {
     constructGraph() {
       this.graph = ForceGraph()(document.getElementById("graph"))
-        .linkDirectionalArrowLength(10)
+        //.linkDirectionalArrowLength(10)
+        .linkWidth(0)
         .nodeCanvasObject(({ id, stats, x, y }, ctx) => {
           ctx.fillStyle = "#555555";
-          ctx.fillRect(x - 18, y - 8, 36, 16);
+          ctx.fillRect(x - 18, y - 16, 36, 16);
           ctx.fillStyle = "#e33636";
           ctx.font = "4px Sans-Serif";
           ctx.textAlign = "center";
           ctx.textBaseline = "middle";
-          ctx.fillText(id, x, y - 4);
+          ctx.fillText(id, x, y - 12);
 
           ctx.fillStyle = "#e33636";
-          ctx.fillRect(x - 18, y, 36, 8);
+          ctx.fillRect(x - 18, y-8, 36, 8);
 
           ctx.fillStyle = "#ffffff";
           ctx.font = "4px Sans-Serif";
           ctx.textAlign = "center";
           ctx.textBaseline = "middle";
-          ctx.fillText(stats.sum, x - 12, y + 4);
-          ctx.fillText(stats.min, x, y + 4);
-          ctx.fillText(stats.max, x + 12, y + 4);
+          ctx.fillText(stats.sum, x - 12, y - 4);
+          ctx.fillText(stats.min, x, y - 4);
+          ctx.fillText(stats.max, x + 12, y -4);
+        })
+        .linkCanvasObjectMode(() => 'after')
+        .linkCanvasObject((link, ctx) => {
+            ctx.lineWidth = 0.5;
+            //ctx.setLineDash([3, 3]);
+            ctx.fillStyle = '#e33636';
+            ctx.fillRect(link.source.x-2, link.source.y, 3, 3);
+            ctx.moveTo(link.source.x, link.source.y);
+            ctx.lineTo(link.target.x, link.target.y);
+            ctx.stroke();
         })
         .nodeLabel("id");
     },
