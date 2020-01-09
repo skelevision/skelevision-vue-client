47d98709fee214922127aea8ff2a01cf336799c3
Hilke <hilke.buss@rwth-aachen.de>
2020-01-09 17:21:43 +0100

    improved link /node positioning added custom links / dummy text

--- src/components/GraphComponent.vue
+++ src/components/GraphComponent.vue
@@ -66,49 +66,93 @@ export default {
 
         .linkCanvasObject((link, ctx) => {            
             //always after rectangle
-            //ctx.fillStyle = '#500555';
-            //ctx.fillRect(link.source.x-2, link.source.y, 4, 4);
+            ctx.fillStyle = '#500555';
+            ctx.fillRect(link.source.x-7, link.source.y, 4, 4);
 
             //always after triangle
-            //ctx.fillStyle = '#555555';
-            //ctx.beginPath(); 
-            //ctx.moveTo(link.target.x-2, link.target.y+8);  
-            //ctx.lineTo(link.target.x+2, link.target.y+8); 
-            //ctx.lineTo(link.target.x, link.target.y+4);
-            //ctx.fill();
+            ctx.fillStyle = '#555555';
+            ctx.beginPath(); 
+            ctx.moveTo(link.target.x-7, link.target.y+8);  
+            ctx.lineTo(link.target.x-3, link.target.y+8); 
+            ctx.lineTo(link.target.x-5, link.target.y+4);
+            ctx.fill();
+
+            //always after draw the actual link
+            ctx.lineWidth = 0.5;
+            ctx.strokeStyle = '#555555';
+            ctx.beginPath();
+            ctx.moveTo(link.source.x-5, link.source.y+8);
+            ctx.lineTo(link.target.x-5, link.target.y+8);
+            ctx.stroke();
 
             //always before rectangle
-            //ctx.fillStyle = '#500555';
-            //ctx.fillRect(link.target.x-2, link.target.y, 4, 4);
+            ctx.fillStyle = '#500555';
+            ctx.fillRect(link.target.x+3, link.target.y, 4, 4);
 
             //always before triangle
-            //ctx.fillStyle = '#555555';
-            //ctx.beginPath(); 
-            //ctx.moveTo(link.target.x-2, link.target.y+8);  
-            //ctx.lineTo(link.target.x+2, link.target.y+8); 
-            //ctx.lineTo(link.target.x, link.target.y+4);
-            //ctx.fill();
+            ctx.fillStyle = '#555555';
+            ctx.beginPath(); 
+            ctx.moveTo(link.target.x+3, link.target.y+8);  
+            ctx.lineTo(link.target.x+7, link.target.y+8); 
+            ctx.lineTo(link.target.x+5, link.target.y+4);
+            ctx.fill();
+
+            //always before draw the actual link
+            ctx.lineWidth = 0.5;
+            ctx.strokeStyle = '#555555';
+            ctx.beginPath();
+            ctx.moveTo(link.source.x+5, link.source.y+8);
+            ctx.lineTo(link.target.x+5, link.target.y+8);
+            ctx.stroke();
 
             //never together rectangles
             ctx.fillStyle = '#000000';
-            ctx.fillRect(link.target.x-2, link.target.y, 4, 4);
-            ctx.fillRect(link.source.x-2, link.source.y, 4, 4);
+            ctx.fillRect(link.target.x-12, link.target.y, 4, 4);
+            ctx.fillRect(link.source.x-12, link.source.y, 4, 4);
+
+            //never together draw the actual link
+            ctx.lineWidth = 0.5;
+            ctx.strokeStyle = '#555555';
+            ctx.beginPath();
+            ctx.moveTo(link.source.x-10, link.source.y+4);
+            ctx.lineTo(link.target.x-10, link.target.y+4);
+            ctx.stroke();
 
             //directly follows circle
-            //ctx.fillStyle = '#500555';
-            //ctx.beginPath(); 
-            //ctx.arc(link.source.x, link.source.y+2, 2.5, 0, 2 * Math.PI, false); 
-            //ctx.fill();
+            ctx.fillStyle = '#500555';
+            ctx.beginPath(); 
+            ctx.arc(link.source.x, link.source.y+2, 2.5, 0, 2 * Math.PI, false); 
+            ctx.fill();
 
-            //cirectly follows triangle
-            //ctx.fillStyle = '#555555';
-            //ctx.beginPath(); 
-            //ctx.moveTo(link.target.x-2, link.target.y+8);  
-            //ctx.lineTo(link.target.x+2, link.target.y+8); 
-            //ctx.lineTo(link.target.x, link.target.y+4);
-            //ctx.fill();
+            //directly follows triangle
+            ctx.fillStyle = '#555555';
+            ctx.beginPath(); 
+            ctx.moveTo(link.target.x-2, link.target.y+8);  
+            ctx.lineTo(link.target.x+2, link.target.y+8); 
+            ctx.lineTo(link.target.x, link.target.y+4);
+            ctx.fill();
 
-            //draw the actual link
+            //directly follows text
+            const start = link.source;
+            const end = link.target;
+            const textPos = Object.assign(...['x', 'y'].map(c => ({
+            [c]: start[c] + (end[c] - start[c]) / 2 // calc middle point
+            })));
+            const relLink = { x: end.x - start.x, y: end.y - start.y };
+            let textAngle = Math.atan2(relLink.y, relLink.x);
+            if (textAngle > Math.PI / 2) textAngle = -(Math.PI - textAngle);
+            if (textAngle < -Math.PI / 2) textAngle = -(-Math.PI - textAngle);
+            ctx.font = "4px Sans-Serif";
+            ctx.save();
+            ctx.translate(textPos.x, textPos.y);
+            ctx.rotate(textAngle);
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.fillStyle = "#000000";
+            ctx.fillText("Text", 0, 0);
+            ctx.restore();
+
+            //directly follows draw the actual link
             ctx.lineWidth = 0.5;
             ctx.strokeStyle = '#555555';
             ctx.beginPath();
